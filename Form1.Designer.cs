6f18f924bb1574f2df06020483304f1081b592e6
Nijest <fantalinaa@mail.ru>
2016-02-24 11:02:22 +0600

    project has been added

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Project_T
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(303, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(59, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(303, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(59, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "расчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 96);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(350, 45);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "N";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "K";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(29, 8);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(268, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(29, 45);
+            this.trackBar2.Maximum = 9;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(268, 45);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 153);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "T";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Project_T
+{
+    public partial class Form1 : Form
+    {
+        BigInteger num;
+        int N = 1, K = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int unhappy_rec(int X, List<int> Number) // рекурсивная функция поиска несчастливых номеров
+        {
+            if (X <= K)
+            {
+                if (Number.Contains(X)) // если номер содержит цифру, равную искомому числу X, номер счастливый
+                { return 0; }
+            }
+            for (int i = 0; i < Number.Count; i++)
+            {
+                int max = Number[i]; // берем самую большую цифру в номере
+                if (max > X) // если она больше искомого числа X, продолжить цикл
+                { continue; }
+                Number.Remove(max); // удаляем эту цифру из номера
+                if (unhappy_rec(X - max, Number) == 0) // рекурс. вызов функции, где искомое число X уменьшено на выбранную цифру
+                    return 0;
+                else while (i < Number.Count && Number[i] == max) // проскакиваем повторения выбранной цифры в этом номере
+                    { i++; }
+                return 1;
+            }
+            return 1;
+        }
+        void add(List<int> Number, int index, int k) // рекурсиваная функция создания следующего номера в указанной системе счисления (K)
+        {
+            if (index < 0)
+                return;
+            if (Number[index] + k <= K) // если изменяется только текущий разряд
+            { Number[index] = Number[index] + k; }
+            else if (Number[index] + k == K + 1) // нужно добавить 1 к разряду более высокого порядка
+            {
+                add(Number, index - 1, 1);
+                for (int i = index; i < Number.Count; i++)
+                    Number[i] = 0;
+            }
+            else if (Number[index] + k > K + 1) // нужно добавить 1 к разряду более высокого порядка и оставшуюся часть к текущему
+            {
+                add(Number, index - 1, 1);
+                for (int i = index; i < Number.Count; i++)
+                    Number[i] = 0;
+                add(Number, index, k - K - 1);
+            }
+        }
+        BigInteger generate() // генерация массива номеров и их обработка
+        {
+            List<int> Number = new List<int> { }; // Номер
+            List<int> SortNumber = new List<int> { }; // Номер, в котором отсортированы цифры
+            for (int j = 0; j < N; j++)
+            { Number.Add(0); } // первый номер состоит из нулей
+            BigInteger n = 0, unh = 0, temp = 0; // n - счетчик обработанных номеров, unh - несчастливых, temp - вспомогательный, для цикла
+            int half; // значение половины суммы цифр номера
+            while (n < num - 1) // пока не обработали все номера
+            {
+                int unh1 = 0; //этот блок if-else заставляет обращаться к переменным типа BigInteger реже
+                if (n + 1000000000 < num - 1) //1 раз в 1 миллиард номеров, так как обращение к ним дольше
+                { // обрабатывается (так себе оптимизация, конечно)
+                    temp = 1000000000;
+                    n += 1000000000;
+                }
+                else
+                {
+                    temp = num - 1 - n;
+                    n = num - 1;
+                }
+                for (int k = 0; k < temp; k++) // обработка номеров
+                {
+                    add(Number, N - 1, 1); // генерируем следующий по порядку номер
+                    if (Number.Sum() % 2 == 1) // если сумма цифр нечетная, он несчастливый
+                    { unh1++; }
+                    else
+                    {
+                        SortNumber = Number.ToList();
+                        SortNumber.Sort();
+                        SortNumber.Reverse(); // на основе номера создали массив отсортированных цифр
+                        half = Convert.ToInt32(SortNumber.Sum() / 2); // половина суммы цифр
+                        unh1 += unhappy_rec(half, SortNumber); // определение, несчастливый номер или нет
+                    }
+                }
+                unh += unh1;
+            }            
+            return unh; // возвращаем кол-во несчастливых номеров
+        }
+        private void button1_Click(object sender, EventArgs e) // расчет задачи
+        {
+            DateTime time1 = DateTime.Now, time2; // таймеры для вывода времени решения задачи
+            richTextBox1.Clear();
+            num = BigInteger.Pow(K + 1, N); // общее число номеров
+            BigInteger k = generate(); // вызов функции создания и обработки массива номеров; k - число несчастливых
+            time2 = DateTime.Now;
+            richTextBox1.Text += "Из общего числа номеров " + (num) + " несчастливыми являются " + k + '\n';
+            richTextBox1.Text += "Расчеты заняли " + (time2 - time1).Minutes.ToString() + "мин " + (time2 - time1).Seconds.ToString() + "c " + (time2 - time1).Milliseconds.ToString() + "мс";            
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e) // задание параметра N
+        {
+            N = trackBar1.Value;
+            if(N*K>300)
+            {
+                K = 300 / N;
+                trackBar2.Value = K;
+            }
+            textBox1.Text = N.ToString();
+            textBox2.Text = K.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e) // задание параметра K
+        {
+            K = trackBar2.Value;
+            if (N * K > 300)
+            {
+                N = 300 / K;
+                trackBar1.Value = N;
+            }
+            textBox1.Text = N.ToString();
+            textBox2.Text = K.ToString();
+        }
+    }
+}
